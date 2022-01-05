ba22f4fefa198dbbc87abc860b3cd07d3d0c1a2a
Aitsam Ahad <aitsama.ahad@gmail.com>
2022-01-05 22:06:28 +0500

    Added instructional comments.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import AutoComplete from "./components/AutoComplete"
 function App() {
 	return (
 		<div className="App">
+			<h1>React Location AutoComplete</h1>
+			<p>Enter postcode, city or any UK based address. Example: sk56rs, blackburn, hardman etc</p>
+
 			<AutoComplete />
 		</div>
 	)
