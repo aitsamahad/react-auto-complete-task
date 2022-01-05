301cbe2f903c7c5e3251d41d1d20eb8142228bef
Aitsam Ahad <aitsama.ahad@gmail.com>
2022-01-05 22:01:13 +0500

    Added try/catch and cancelToken for location search api.

--- src/api/location.ts
+++ src/api/location.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+let cancelToken
 
 type SearchResponse = {
 	id: string
@@ -11,8 +12,18 @@ type SearchResponse = {
 }
 
 export const searchLocation = async (query: string): Promise<SearchResponse[]> => {
-	const { status, data } = await axios.get(`https://aitsamahad.dev/api/location?search=${query}`)
+	if (typeof cancelToken != typeof undefined) {
+		cancelToken.cancel("Aborting previous req.")
+	}
+	cancelToken = axios.CancelToken.source()
+	try {
+		const { status, data } = await axios.get(`https://aitsamahad.dev/api/location?search=${query}`, {
+			cancelToken: cancelToken.token,
+		})
 
-	if (status === 200) return data?.data
-	return []
+		if (status === 200) return data?.data
+		return []
+	} catch (err) {
+		return []
+	}
 }
